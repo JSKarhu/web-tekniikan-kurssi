ac8edf9e21b6e2dd7979019a681c693c62873c52
JSKarhu <92786584+JSKarhu@users.noreply.github.com>
2021-12-09 13:28:29 +0200

    luotiin uudet html sekä javascript tiedostot

---
+++ css/harjoitus6/code.js
@@ -0,0 +1,19 @@
+// Let's create a HTTP request
+let xmlhttp = new XMLHttpRequest();
+xmlhttp.open("GET",
+"https://yesno.wtf/api/",true);
+// Send request
+xmlhttp.send();
+
+// Let's create an event handler
+xmlhttp.onreadystatechange=function() {
+    // Now an onreadystatechange event has occurred
+    if(xmlhttp.readyState === 4 && xmlhttp.status === 200){
+
+        // Response is ready
+        let answerObject = JSON.parse(xmlhttp.response);
+        document.getElementById("answer").innerHTML = answerObject.answer;
+        answerObject.image;
+        document.getElementById("answer-image").setAttribute('src', answerObject.image);
+    }
+}
